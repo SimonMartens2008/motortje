b89e71af4514c5156e8043b72bcbebcdbb15f363
SimonMartens2008 <101873903+SimonMartens2008@users.noreply.github.com>
2022-06-15 11:29:24 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -3,11 +3,11 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     pins.digitalWritePin(DigitalPin.P2, 1)
-    for (let index = 0; index < 15; index++) {
+    for (let index = 0; index < 10; index++) {
         ContinuousServo.spin_one_way_with_speed(AnalogPin.P2, 10)
-        basic.pause(2150)
+        basic.pause(1000)
         ContinuousServo.spin_other_way_with_speed(AnalogPin.P2, 10)
-        basic.pause(2050)
+        basic.pause(1000)
     }
     pins.digitalWritePin(DigitalPin.P2, 0)
     ContinuousServo.turn_off_motor(DigitalPin.P1)
